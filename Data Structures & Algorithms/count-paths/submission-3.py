74622b6b3b0af9a93e32bade3b22692d928c44d7
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2026-06-05 20:24:55 +0100

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = defaultdict(int)
+        def solve(i,j):
+            if i == 0 or j == 0: return 1
+            if (i,j) in memo: return memo[(i,j)]
+            memo[(i,j)] = solve(i-1,j) + solve(i,j-1)
+            return memo[(i,j)]
+        return solve(m-1,n-1)
